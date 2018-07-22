b0c4048dc58e0e25bcfea3a57f421520f4eac5ba
FredericoSilva015 <fredsilva015@gmail.com>
2018-07-22 14:53:26 +0100

    created guardian api playground to better understand it

--- react-news/src/containers/homepage/homepage.js
+++ react-news/src/containers/homepage/homepage.js
@@ -1,21 +1,90 @@
 import React, { Component } from 'react';
-import logo from '../../assets/logo.svg';
+// import logo from '../../assets/logo.svg';
 import './homepage.css';
 
-class homepage extends Component {
+class Homepage extends Component {
+  /** 
+   * @constructor for Homepage component
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: [],
+    }
+  }
+
+
+  /** 
+   * fetch the Data from the guardian API
+   */
+  componentDidMount() {
+    fetch('https://content.guardianapis.com/search?api-key=3a252dbd-528c-4214-9211-a89fcfb96697')
+    .then(res => res.json())
+    .then(
+      // on success 
+      (result) => {
+        this.setState({
+          data: result.response.results,
+          isLoaded : true,
+        });
+        // dev to map the entire api
+        console.log('fetch: ',result.response);
+      },
+      // on error
+      (error) => {
+        this.setState({
+          isLoaded : true,
+          error,
+        })
+      }
+    )
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+    /** 
+     * multiple @constant
+     * points to each @argument {state} 
+     */
+     const { error, isLoaded, data} = this.state;
+
+    // dev purposes to check data
+    console.log('state: ',data);
+
+    /** 
+     * we creat the temporary data block
+     * we render 3 times for the time being
+     * receives @const {error} 
+     */
+    if (error) {
+      return (
+        <div className="App">
+          Error: {error.message}
+        </div>
+      );
+    } 
+    else if (!isLoaded) {
+      return (
+        <div className="App">
+            Loading...
+        </div>
+      );
+    }
+    else {
+      // just wish to see some data
+      return (
+        <div className="App">
+          <ul>
+            { data.map((data) => {  
+                return <li key={data.id}>{data.id}</li>
+              })}
+          </ul>
+        </div>
+      );
+    }
   }
 }
 
-export default homepage;
+export default Homepage;
